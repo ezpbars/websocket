88fce4323f1bb3c0153fe84174accb47e8d75129
moorexjoy <109114632+moorexjoy@users.noreply.github.com>
2022-10-27 10:44:33 -0700

    Traces (#2)
    * add core watch endpoint
    
    * fixes for the integration tests

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 import multiprocessing
+import progress_bars.router
 import updater
 
 multiprocessing.Process(target=updater.listen_forever_sync, daemon=True).start()
 app = FastAPI(title="ezpbars", description="easy progress bars", version="1.0.0+alpha")
+
+app.include_router(
+    progress_bars.router.router, prefix="/api/2/progress_bars", tags=["progress_bars"]
+)
+
+
+@app.websocket("/api/2/test/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
 
 
 @app.get("/")
